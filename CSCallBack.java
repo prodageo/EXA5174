100180691fa5133d71e6a4d444937353e5d799f7
frederic-baucher <frederic.baucher@insa-rouen.fr>
2021-04-02 18:43:49 +0200

    Create CSCallBack.java

---
+++ CSCallBack.java
@@ -0,0 +1,9 @@
+
+/**
+ * @author Jeff Lee
+ * @since 2015-10-21 21:34:21
+ *  The callback mode - Calls back to the interface class 
+ */
+public interface CSCallBack {
+  public void process(String status);
+}
